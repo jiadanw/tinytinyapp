7135eb9e1651389e22da2180184e7a7f0a92abda
Jiadan <jiadanwang@gmail.com>
2019-10-21 20:41:48 +0000

    add _header partial

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -15,4 +17,32 @@ res.send("Hello");
 app.listen(PORT, () => {
   console.log(`Wangjiadan app listening on port ${PORT}`)
 });
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  let templateVars = {greeting:'Hello World 777'};
+  res.render("hello_world", templateVars);
+});
+
+app.get("/set",(req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`)
+})
+
+app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+ });
+
+app.get("/urls",(req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL",(req, res) => {
+  let templateVars = {shortURL : req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
+  res.render("urls_show", templateVars);
+})
 
